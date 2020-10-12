b79fbbb99869d7677635d9ed394a3d85757e46c6
Kai Adrian Reid-Casey <kaiadrianreidcasey@gmail.com>
2020-10-12 00:29:42 -0400

    Home Screen: Added stub for button action.

--- app/src/main/java/com/kaireidcasey/uslawquiz/MainActivity.java
+++ app/src/main/java/com/kaireidcasey/uslawquiz/MainActivity.java
@@ -3,6 +3,7 @@ package com.kaireidcasey.uslawquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button startBtn = findViewById(R.id.btnStart);
+//        startBtn.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View view) {
+//                // Code here executes on main thread after user presses button
+//                //view.addthings
+//            }
+//        });
     }
 }
